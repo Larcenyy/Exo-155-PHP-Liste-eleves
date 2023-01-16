b24e94e517b626096e0a0e6844cdb8d63ca0a8dd
Rémy <remyc59440@hotmail.com>
2023-01-16 13:00:37 +0100

    dx

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+$myArray = [
+    0 => ["Comeau", "Rémy", "19", "Landrecies", "Passions" => ["Dev, musique, sortir"]],
+    1 => ["Brian", "Bertrand", "29", "London", "Passions" => ["Dev, musique, sortir"]],
+    2 => ["Bonn", "Benoit", "17", "Paris", "Passions" => ["Dev, musique, me branler"]],
+    3 => ["Doe", "John", "25", "Avesne", "Passions" => ["Dev, musique, sortir"]],
+];
---
+++ index.php
@@ -0,0 +1,15 @@
+
+<?php
+    require "db.php"
+?>
+
+<ul class="list">
+    <?php
+        foreach ($myArray as $key => $value){
+            echo "Le prénom de l'étudiant est : " . "<strong>$value[0]</strong>" . "<br>";
+            echo "Le nom de l'étudiant est : " . "<strong>$value[1]</strong>" . "<br>";
+            echo "Accès aux informations : " . "<strong><a href='info.php?perso=$key'>ICI</a></strong>" . "<br>";
+            echo "<hr>";
+        }
+    ?>
+</ul>
